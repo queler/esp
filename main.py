c1edc65145d16c99d209a7f33840ead1bb4df75e
q <queler@gmail.com>
2025-12-02 15:25:17 -0500

    abandoning

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# main.py
+# main.pycomp
 import os
 
 AUTO = "autorun.flag"
-
-if AUTO in os.listdir():
-    print("AUTO MODE ENABLED — running realmain.")
+def _run():
+    """Start the main application in auto mode."""
     import realmain
     import uasyncio
     uasyncio.run(realmain.run())
+if AUTO in os.listdir():
+    print("AUTO MODE ENABLED — running realmain.")
+    _run()
 else:
     print("SAFE MODE — nothing auto-run.")
     print("You can now: mpremote soft-reset; mpremote mount . ; exec realmain")
