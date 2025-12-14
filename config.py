32341e29c812bc0a7276935963ff8b6ce4eb3ee2
Adam Queler <queler+k@gmail.com>
2025-12-13 22:52:17 -0500

    1 sec poll, fixed text

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ NTP_HOST = "pool.ntp.org"
 TIMEZONE_OFFSET_MINUTES = 0  # adjust if you want local time
 
 # How often ModeManager should re-evaluate mode (seconds)
-MODE_POLL_INTERVAL = 30
+MODE_POLL_INTERVAL = 1
 
 # How often StatusManager updates blink codes (seconds)
 STATUS_TICK_INTERVAL = 0.2
